4dbe3246947c82c3982b33d67abe00cf672d57bf
ahmedwaelalkaliby <ahmedalkaliby@gmail.com>
2026-03-29 00:14:58 +0200

    solve production error 2

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import SearchPageContent from "../../(_features)/Pages Components/Search/SearchPageContent";
+import SearchPageContent from "../../(_features)/Pages Components/search/SearchPageContent";
 
 export default function SearchPage() {
   return <SearchPageContent />;
